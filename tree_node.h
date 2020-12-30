06ecc6a58b5eb317cebb545b1f1708b339762fc8
Guy Shamilyan <shaguy3@gmail.com>
2020-12-30 09:44:32 +0200

    updated tree_node.h

--- tree_node.h
+++ tree_node.h
@@ -18,15 +18,19 @@ public:
 	TreeNode();
 	TreeNode(Pair _pair, TreeNode* left, TreeNode* right);
 	~TreeNode();
+
 	/* getters */
 	Pair getPair() const { return pair; }
 	TreeNode* Left() const { return left; }
 	TreeNode* Right() const { return right; }
+	
 	/* setters */
 	bool setPair(Pair _pair) { pair.frequency = _pair.frequency; \
 							   pair.charecter = _pair.charecter; \
 							   return true; }
 	bool setLeft(TreeNode* node) { left = node; return true; }
 	bool setRight(TreeNode* node) { right = node; return true; }
+
+	/* prints */
 	void inOrder();
 };
